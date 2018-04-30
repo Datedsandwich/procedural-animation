0562463e48669f1a642a8a32df9c17a55bf7eb51
Datedsandwich <robinsonlm@live.co.uk>
2018-04-30 13:10:38 +0100

    Robot Arm Inverse Kinematics

---
+++ Assets/Scripts/RobotJoint.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotJoint : MonoBehaviour
+{
+    public Vector3 Axis;
+    public Vector3 StartOffset;
+    public float MinAngle;
+    public float MaxAngle;
+
+    [Range(0, 1f)]
+    public float SlowdownThreshold = 0.5f;
+    [Range(0, 360f)]
+    public float Speed = 1f;
+
+    void Awake()
+    {
+        StartOffset = transform.localPosition;
+    }
+
+    public float ClampAngle(float angle, float delta = 0)
+    {
+        return Mathf.Clamp(angle + delta, MinAngle, MaxAngle);
+    }
+
+    public float GetAngle()
+    {
+        float angle = 0;
+        if (Axis.x == 1) angle = transform.localEulerAngles.x;
+        else
+        if (Axis.y == 1) angle = transform.localEulerAngles.y;
+        else
+        if (Axis.z == 1) angle = transform.localEulerAngles.z;
+
+        return ClampAngle(angle);
+    }
+
+    public float SetAngle(float angle)
+    {
+        angle = ClampAngle(angle);
+		
+        if (Axis.x == 1)
+        {
+            transform.localEulerAngles = new Vector3(angle, 0, 0);
+        }
+        else if (Axis.y == 1)
+        {
+            transform.localEulerAngles = new Vector3(0, angle, 0);
+        }
+        else if (Axis.z == 1)
+        {
+            transform.localEulerAngles = new Vector3(0, 0, angle);
+        }
+
+        return angle;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Procedural Animation Experiments
+I've always liked animation, and on a previous project I got to experiment a bit with Inverse Kinematics. I'm doing more of that, here.
+
+Robot Arm Model by [Petr P](https://3dwarehouse.sketchup.com/model/92e064854f4dd504c8ab9067fbd9681d/Robotic-Arm-stainless-steel-R6Stainless).
