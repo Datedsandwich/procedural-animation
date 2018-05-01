b05f56370798978cd8afee3690b436ec22835384
Datedsandwich <robinsonlm@live.co.uk>
2018-05-01 11:22:06 +0100

    Refactoring in preparation for other types of IK, added a tentacle mesh

--- Assets/Scripts/RobotArm.cs
+++ Assets/Scripts/RobotArm.cs
@@ -2,29 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RobotArm : MonoBehaviour
+public class RobotArm : InverseKinematics
 {
-    public RobotJoint[] Joints;
-
-    private float[] Angles = null;
-    public Transform Target;
-    [Tooltip("Distance end effector should maintain to target")]
-    public float TargetDistance;
-    private Vector3 targetPosition;
-
-    [Range(0, 1f)]
-    [Tooltip("Used to simulate gradient (degrees).")]
-    public float DeltaGradient = 0.1f;
-    [Range(0, 100f)]
-    [Tooltip("How much we move depending on the gradient.")]
-    public float LearningRate = 25f;
-
-    [Range(0, 0.25f)]
-    [Tooltip("If closer than this, it stops.")]
-    public float StopThreshold = 0.1f;
-    [Range(0, 10f)]
-    public float SlowdownThreshold = 0.25f;
-
     void Start()
     {
         Angles = new float[Joints.Length];
@@ -41,67 +20,5 @@ public class RobotArm : MonoBehaviour
 
         Debug.DrawLine(Joints[Joints.Length - 1].transform.position, targetPosition, Color.green);
         Debug.DrawLine(Target.transform.position, targetPosition, new Color(0, 0.5f, 0));
-    }
-
-    public float DistanceFromTarget(Vector3 target, float[] angles)
-    {
-        Vector3 point = ForwardKinematics(angles);
-        return Vector3.Distance(point, target);
-    }
-
-    private void ApprochTarget(Vector3 target)
-    {
-        // Starts from the end, down to the base
-        for (int i = Joints.Length - 1; i >= 0; i--)
-        {
-            if (DistanceFromTarget(target, Angles) <= StopThreshold)
-            {
-                break;
-            }
-
-            float error = DistanceFromTarget(target, Angles);
-            float slowdown = Mathf.Clamp01((error - StopThreshold) / (SlowdownThreshold - StopThreshold));
-
-            // Gradient descent
-            float gradient = CalculateGradient(target, Angles, i, DeltaGradient);
-            Angles[i] -= LearningRate * gradient * slowdown;
-            // Clamp
-            Angles[i] = Joints[i].ClampAngle(Angles[i]);
-        }
-
-        for (int i = 0; i < Joints.Length - 1; i++)
-        {
-            Joints[i].SetAngle(Angles[i]);
-        }
-    }
-
-    private float CalculateGradient(Vector3 target, float[] angles, int i, float delta)
-    {
-        float[] anglesCopy = (float[])angles.Clone();
-        float f_x = DistanceFromTarget(target, anglesCopy);
-
-        anglesCopy[i] += delta;
-        float f_x_plus_h = DistanceFromTarget(target, anglesCopy);
-
-        float gradient = (f_x_plus_h - f_x) / delta;
-
-        return gradient;
-    }
-
-    private Vector3 ForwardKinematics(float[] angles)
-    {
-        Vector3 prevPoint = Joints[0].transform.position;
-        Quaternion rotation = Quaternion.identity;
-
-        for (int i = 1; i < Joints.Length; i++)
-        {
-            // Rotates around a new axis
-            rotation *= Quaternion.AngleAxis(angles[i - 1], Joints[i - 1].Axis);
-            Vector3 nextPoint = prevPoint + rotation * Joints[i].StartOffset;
-
-            prevPoint = nextPoint;
-        }
-
-        return prevPoint;
     }
 }
